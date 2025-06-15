b190a5a9933094ba0a6a0621fa5b18753f1f703a
pe3iva <dawgsofast@protonmail.com>
2025-06-15 19:39:18 +0200

    add space between outputs to make it cleaner

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,28 @@
 
 int main() {
 
-	std::cout << "Welcome to the calculator!\n";
+	std::cout << '\n';
+
+	std::cout << "Welcome to the calculator!\n\n";
 	
 	std::cout << "Enter the first number: ";
 	double num1{};
 	std::cin >> num1;
 
+	std::cout << '\n' << '\n';
 
 	std::cout << "Enter the second number: ";
 	double num2{};
 	std::cin >> num2;
 
+	std::cout << '\n' << '\n';
 
 	std::cout << "Enter the operator (+, -, *, /): ";
 	char op{};
 	std::cin >> op;
 	
+	std::cout << '\n';
+
 	switch (op) {
 
 		case '+':
@@ -39,5 +45,4 @@ int main() {
 		default:
 			std::cout << "You entered the an invalid operator.\n";
 	}
-
 }
